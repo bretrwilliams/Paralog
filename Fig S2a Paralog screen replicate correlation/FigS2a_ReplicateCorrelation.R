90c6f55d01e7bbd88c0ab4dae7dde1a067e09749
Ruitong Li <liruitong1104@163.com>
2021-08-30 09:04:06 -0400

    Correlation at LFC not count level

--- Fig S2a Paralog screen replicate correlation/FigS2a_ReplicateCorrelation.R
+++ Fig S2a Paralog screen replicate correlation/FigS2a_ReplicateCorrelation.R
@@ -1,11 +1,13 @@
 Model <- readRDS("Data/LatestModel.rds")
-countsRep <- cor(Model$Input$counts[,-1], method = 'pearson')
+inputRep <- Model$Input
+inputRep$replicate.map$samplename <- inputRep$replicate.map$colname
+inputRep%<>%gemini::gemini_calculate_lfc(Input = ., sample.column.name = 'samplename')
+LFCRepCors <- cor(inputRep$LFC, method = 'pearson')
 ori_Name <- c("REP","Meljuso","TIGI1_004","TIPK1","TIMEL202_003","HS944")
 sub_Name <- c("Rep","MELJUSO","GI1","PK1","MEL202","HS944T")
-colnames(countsRep)%<>%mgsub::mgsub(.,ori_Name,sub_Name)
-rownames(countsRep)%<>%mgsub::mgsub(.,ori_Name,sub_Name)
-sort(rownames(countsRep))
+colnames(LFCRepCors)%<>%mgsub::mgsub(.,ori_Name,sub_Name)
+rownames(LFCRepCors)%<>%mgsub::mgsub(.,ori_Name,sub_Name)
 pdf(paste0("Fig S2a Paralog screen replicate correlation/FigS2a_Replicate_LFC_correlation_",Sys.Date(),".pdf"), width = 10, height = 9)
-pheatmap::pheatmap(mat = countsRep,
+pheatmap::pheatmap(mat = LFCRepCors,
                    color = colorRampPalette(c("white","gray", "#FFAAAA", "#FF6666"))(100))
 dev.off()
